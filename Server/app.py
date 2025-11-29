2513ba8acbb26cf43dcabf5c435e62f3adc91b57
BobrikovMikhail <mihapir4t@yandex.ru>
2025-11-29 14:11:40 +0500

    реализация сервер

---
+++ Server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({
+        "status": "ok",
+        "timestamp": datetime.utcnow().isoformat()
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
