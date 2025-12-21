e192aa34aa1f60d4b704f1309ac7d0edab433980
ValeryShilov <valera.shilov.03@mail.ru>
2025-12-22 01:33:01 +0500

    Добавлен экран текущей погоды

--- AndroidClient/app/src/main/java/ui/viewmodel/WeatherViewModel.java
+++ AndroidClient/app/src/main/java/ui/viewmodel/WeatherViewModel.java
@@ -1,6 +1,7 @@
 package ui.viewmodel;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import data.model.CurrentWeather;
@@ -15,6 +16,7 @@ public class WeatherViewModel extends ViewModel {
     private LiveData<CurrentWeather> currentData;
     private LiveData<ForecastData> forecastData;
     private LiveData<String> error;
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
 
     public WeatherViewModel() {
         WeatherRepository repo = new WeatherRepository();
@@ -23,8 +25,10 @@ public class WeatherViewModel extends ViewModel {
     }
 
     public void loadCurrentWeather(double lat, double lon) {
+        isLoading.setValue(true);
         currentData = getCurrentUseCase.execute(lat, lon);
         error = getCurrentUseCase.getError();
+        isLoading.setValue(false);
     }
 
     public void loadForecast(double lat, double lon) {
@@ -42,6 +46,10 @@ public class WeatherViewModel extends ViewModel {
 
     public LiveData<String> getError() {
         return error;
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
     }
 
 }
